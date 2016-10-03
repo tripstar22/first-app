7c5599f4b373e06a2284e7974f72f23136be5310
Trip Pruitt <trip@iconologic.com>
2016-10-03 16:01:50 -0400

    added functionality to help understand lifecyle of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
 				<h1>Root Component</h1>
-				<fa-lifecycle *ngIf="!delete"></fa-lifecycle>
+				<fa-lifecycle *ngIf="!delete" [bindable]="boundValue">
+					<p>{{test}}</p>
+				</fa-lifecycle>
 				<button (click)="delete = true">Click to Delete</button>
+				<button (click)="test = 'Changed value'">Click to Change Content</button>
+				<button (click)="boundValue = 2000">Click to Change Binding</button>
   `
 })
 export class AppComponent {
   delete = false;
+  test = 'Starting value';
+  boundValue = 1000;
 }
